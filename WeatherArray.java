71549ef7154ccec80bfad421a3e13f1621277047
Yaswanth1323 <yaswanth.gannamani@sasi.ac.in>
2024-01-31 15:02:12 +0530

    commit

---
+++ WeatherArray.java
@@ -0,0 +1,9 @@
+public class WeatherArray
+{
+String str="hello";
+public static void main(String args[])
+{
+WeatherArray obj=new WeatherArray(); //creating object using new keyword
+System.out.println(obj.str);
+}
+}
